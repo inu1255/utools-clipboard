056e3b2b0eb6c3b8f77c4db0ba36c2cada0c2081
inu1255 <929909260@qq.com>
2022-04-08 17:38:03 +0800

    功能完善

--- src/preload.js
+++ src/preload.js
@@ -2,28 +2,103 @@ const fs = require("fs-extra");
 const path = require("path");
 const {clipboard} = require("electron");
 const crypto = require("crypto");
-const {PrismaClient} = require("./prisma-client");
+const clipboardListener = require("clipboard-event");
+const yaml = require("yaml");
 const home = utools.getPath("home");
-const prisma = new PrismaClient();
+const config = {
+	limit: 5000, // 限制历史条数
+	large_limit: 100, // 图片和大文字限制
+	large_text: 512, // 图片超过多少字节算作大文件
+	snippet_path: path.join(home, ".snippets.yaml"),
+};
+const config_path = path.join(home, ".utools-clipboard.yaml");
+try {
+	Object.assign(config, yaml.parse(fs.readFileSync(config_path, "utf8")));
+} catch (error) {}
+fs.writeFile(config_path, yaml.stringify(config));
 window.require = require;
 window.__WorkDir = __dirname;
-let historys = [];
-let snippets = [];
+let historys;
+let snippets;
 utools.onPluginReady(refreshHistory);
 utools.onDbPull(refreshHistory);
+// To start listening
+clipboardListener.startListening();
+
+class Db {
+	allDocs() {
+		return utools.db.allDocs();
+	}
+
+	remove(id) {
+		return utools.db.remove(id);
+	}
+
+	put(item) {
+		return utools.db.put(item);
+	}
+}
+
+const db = new Db();
 
 function refreshHistory() {
-	let items = utools.db.allDocs();
+	let items = db.allDocs();
 	items.sort((a, b) => {
 		return b.time - a.time;
 	});
-	historys.length = 0;
+	historys = [];
 	for (let item of items) {
 		historys.push(itemMap(item));
 	}
 	console.log("初始化条数", items.length);
 	window.exports.clipboard.args.placeholder = "搜索(" + historys.length + ")条";
 }
+
+let currentClipboardText = "";
+function pbpaste() {
+	let file;
+	// if (utools.isWindows()) {
+	// 	const rawFilePath = clipboard.readBuffer("FileNameW").toString("ucs2");
+	// 	file = rawFilePath.replace(new RegExp(String.fromCharCode(0), "g"), "");
+	// } else if (remote.process.platform == "darwin") {
+	// 	file = clipboard.read("public.file-url").replace("file://", "");
+	// }
+	if (file) return {type: "file", data: file};
+	let image = clipboard.readImage();
+	let size = image.getSize();
+	if (!image.isEmpty())
+		return {type: "image", size: `${size.width}x${size.height}`, data: image.toDataURL()};
+	let text = clipboard.readText();
+	currentClipboardText = text;
+	if (text.trim()) return {type: "text", data: text};
+}
+
+clipboardListener.on("change", () => {
+	let item = pbpaste();
+	if (!item) return;
+	if (!historys) refreshHistory();
+	item._id = crypto.createHash("md5").update(item.data).digest("hex");
+	item.time = Date.now();
+	for (let i = 0; i < historys.length; i++) {
+		let x = historys[i];
+		if (x._id == item._id) {
+			historys.splice(i--, 1);
+		}
+		if (i > config.large_limit && x.large) {
+			historys.splice(i--, 1);
+			db.remove(x._id);
+			console.log("内容太大删除", i, x);
+		}
+	}
+	historys.unshift(itemMap(item));
+	window.exports.clipboard.args.placeholder = "搜索(" + historys.length + ")条";
+	db.put(item);
+	if (historys.length > config.limit) {
+		let x = historys.pop();
+		db.remove(x._id);
+		console.log("超过上线删除", historys.length, x);
+	}
+});
 
 function timestamp(t) {
 	if (!t) t = new Date();
@@ -41,27 +116,49 @@ function timestamp(t) {
 	return `${year}-${month}-${date} ${hours}:${mintues}:${seconds}`;
 }
 
+const bs = ["b", "kb", "mb", "gb", "tb", "pb", "eb", "zb"];
+/**
+ * 字节数转换
+ * @param {number} size
+ * @param {number} [n=0] 保留几位小数
+ * @param {number} [i=0] 从什么开始 b/kb/mb/gb
+ * @returns
+ */
+function traffic(size, n = 2, i = 0) {
+	if (size == null) return "";
+	size = parseInt(size);
+	for (; i < bs.length; i++) {
+		if (size < 1024) {
+			return size + bs[i];
+		}
+		size = (size / 1024).toFixed(n);
+	}
+	return size + "YB";
+}
+
 function itemMap(x) {
 	if (x.type == "image")
 		return {
 			_id: x._id,
-			title: "[" + (/image\/\w+/.exec(x.data) || ["image"])[0] + "]",
-			description: timestamp(new Date(x.time)),
+			title: x.size || "[" + (/image\/\w+/.exec(x.data) || ["image"])[0] + "]",
+			description: timestamp(new Date(x.time)) + " " + traffic(Math.floor(x.data.length * 0.75)),
 			icon: x.data,
 			large: true,
 			click() {
 				utools.copyImage(x.data);
+				return true;
 			},
 		};
 	if (x.type == "text")
 		return {
 			_id: x._id,
-			title: x.data,
-			description: timestamp(new Date(x.time)),
+			title: x.data.slice(0, 128),
+			description: timestamp(new Date(x.time)) + " " + traffic(x.data.length),
 			icon: "res/text.svg",
-			large: x.data.length > 512,
+			large: x.data.length > config.large_text,
 			click() {
 				utools.copyText(x.data);
+				return true;
 			},
 		};
 	if (x.type == "file")
@@ -73,8 +170,24 @@ function itemMap(x) {
 			large: false,
 			click() {
 				utools.copyFile(x.data);
+				return true;
 			},
 		};
+}
+
+function saveSnippet() {
+	fs.writeFile(config.snippet_path, yaml.stringify(snippets));
+}
+
+function loadSnippet(reload) {
+	if (snippets && !reload) return Promise.resolve(snippets);
+	return fs.readFile(config.snippet_path, "utf8").then(
+		function (text) {
+			snippets = yaml.parse(text);
+			return snippets;
+		},
+		() => ({})
+	);
 }
 
 window.exports = {
@@ -82,48 +195,90 @@ window.exports = {
 		mode: "list",
 		args: {
 			enter: (action, callbackSetList) => {
-				fs.readJSON(path.join(home, ".snippets.json"))
-					.then(
-						(items) => {
-							snippets.length = 0;
-							for (let item of items) {
-								item.icon = "res/snippet.svg";
-								item.click = function () {
-									utools.copyText(item.data || item.description);
-								};
-								snippets.push(item);
-							}
-						},
-						function () {}
-					)
-					.then(function () {
-						callbackSetList(historys);
-					});
+				if (!historys) refreshHistory();
+				callbackSetList(historys);
 			},
 			search: (action, searchWord, callbackSetList) => {
-				let results = historys;
+				let pms = Promise.resolve(historys);
 				if (searchWord) {
 					searchWord = searchWord.toLowerCase();
-					results = results.filter((x) => {
+					let results = historys.filter((x) => {
 						return x.title && ~x.title.toLowerCase().indexOf(searchWord);
 					});
+					pms = Promise.resolve(results);
 					if (searchWord.startsWith("s ")) {
-						let key = searchWord.slice(2);
-						results = snippets
-							.filter((x) => {
-								return x.title && ~x.title.toLowerCase().indexOf(key);
-							})
-							.concat(results);
+						pms = loadSnippet(true).then((data) => {
+							let list = [];
+							// 添加snippet
+							if (snippets && /^s add \w+/.test(searchWord)) {
+								let keys = searchWord.slice(6).trim().split(" ");
+								for (let i = 0; i < keys.length; i++) {
+									let title = keys.slice(0, i + 1).join(" ");
+									let description = keys.slice(i + 1).join(" ");
+									if (!description) {
+										if (!currentClipboardText) pbpaste();
+										description = currentClipboardText;
+									}
+									if (description) {
+										list.push({
+											title,
+											description,
+											icon: "res/add.svg",
+											click() {
+												snippets[title] = description;
+												saveSnippet();
+											},
+										});
+									}
+								}
+							}
+							// 删除snippet
+							if (snippets && /^s del \w+/.test(searchWord)) {
+								let key = searchWord.slice(6).trim().split(" ");
+								for (let title in snippets) {
+									if (title.toLowerCase().indexOf(key) >= 0) {
+										let description = snippets[title];
+										list.push({
+											title,
+											description,
+											icon: "res/delete.svg",
+											click() {
+												delete snippets[title];
+												saveSnippet();
+											},
+										});
+									}
+								}
+							}
+							// 搜索snippet
+							let key = searchWord.slice(2);
+							for (let title in data) {
+								if (title.toLowerCase().indexOf(key) >= 0) {
+									let description = data[title];
+									list.push({
+										title,
+										description,
+										icon: "res/snippet.svg",
+										click() {
+											utools.copyText(description);
+											return true;
+										},
+									});
+								}
+							}
+							return list.concat(results);
+						});
 					}
 				}
-				callbackSetList(results);
+				pms.then(callbackSetList, () => callbackSetList(historys));
 			},
 			select: (action, itemData, callbackSetList) => {
 				window.utools.hideMainWindow();
 				utools.setSubInputValue("");
 				window.utools.outPlugin();
-				itemData.click();
-				utools.simulateKeyboardTap("v", utools.isMacOs() ? "command" : "ctrl");
+				if (itemData.click()) {
+					utools.simulateKeyboardTap("v", utools.isMacOs() ? "command" : "ctrl");
+				}
 			},
 			placeholder: "搜索",
 		},
